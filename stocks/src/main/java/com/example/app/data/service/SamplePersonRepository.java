0a8ec3bb001ea0ebeafbcab96dd0b32653a01169
zeeshan <zeeshanm2405@gmail.com>
2022-10-16 16:06:03 +0100

    new project commit
    testing files

---
+++ stocks/src/main/java/com/example/app/data/service/SamplePersonRepository.java
@@ -0,0 +1,9 @@
+package com.example.app.data.service;
+
+import com.example.app.data.entity.SamplePerson;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SamplePersonRepository extends JpaRepository<SamplePerson, UUID> {
+
+}
