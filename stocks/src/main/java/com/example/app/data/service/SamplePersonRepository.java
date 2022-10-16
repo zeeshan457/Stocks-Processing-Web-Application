7e80fe1325985964457950abbfc6a729f3c8d443
zeeshan <zeeshanm2405@gmail.com>
2022-10-16 16:13:35 +0100

    delete project commit
    testing files

--- stocks/src/main/java/com/example/app/data/service/SamplePersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.app.data.service;
-
-import com.example.app.data.entity.SamplePerson;
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SamplePersonRepository extends JpaRepository<SamplePerson, UUID> {
-
-}
