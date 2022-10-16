71c3b2f16aebfda2a4dca4aa065ec3fb0aefdf8e
zeeshan <zeeshanm2405@gmail.com>
2022-10-16 16:30:49 +0100

    new project commit
    testing these files

---
+++ stocks/src/main/java/com/example/app/views/home/HomeView.java
@@ -0,0 +1,35 @@
+package com.example.app.views.home;
+
+import com.example.app.views.MainLayout;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+
+@PageTitle("Home")
+@Route(value = "Home", layout = MainLayout.class)
+@RouteAlias(value = "", layout = MainLayout.class)
+public class HomeView extends HorizontalLayout {
+
+    private TextField name;
+    private Button sayHello;
+
+    public HomeView() {
+        name = new TextField("Your name");
+        sayHello = new Button("Say hello");
+        sayHello.addClickListener(e -> {
+            Notification.show("Hello " + name.getValue());
+        });
+        sayHello.addClickShortcut(Key.ENTER);
+
+        setMargin(true);
+        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+
+        add(name, sayHello);
+    }
+
+}
